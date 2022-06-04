5525c3cede268be6225bb4b942c65ed0e5122baf
subairdc <subairdc@gmail.com>
2022-06-05 00:32:08 +0530

    Project Setup Components Creation

--- Angular/HostelManagement/src/app/app.module.ts
+++ Angular/HostelManagement/src/app/app.module.ts
@@ -7,12 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './Material/material.module';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { StudentLoginComponent } from './Components/login/student-login/student-login.component';
+import { AdminLoginComponent } from './Components/login/admin-login/admin-login.component';
+import { StudentDashboardComponent } from './Components/student/student-dashboard/student-dashboard.component';
+import { StaffDashboardComponent } from './Components/staff/staff-dashboard/staff-dashboard.component';
+import { AdminDashboardComponent } from './Components/admin/admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    StudentLoginComponent,
+    AdminLoginComponent,
+    StudentDashboardComponent,
+    StaffDashboardComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,
