b964a9d8ca6e0d319b22f25a684fa960aee77f6f
subairdc <subairdc@gmail.com>
2022-07-20 12:01:10 +0530

    staff details popup page

--- Angular/HostelManagement/src/app/Components/staff/staff-homepage/staff-homepage.component.ts
+++ Angular/HostelManagement/src/app/Components/staff/staff-homepage/staff-homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-staff-homepage',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaffHomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route : Router) { }
 
   ngOnInit(): void {
   }
+
+  logout() {
+    localStorage.removeItem("token");
+    this.route.navigate(['/staffLogin']);
+  }
+
 
 }
